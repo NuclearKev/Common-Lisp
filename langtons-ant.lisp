143f66065de9bdb22b3cf7b853e27a3dc8524624
Kevin "The Nuclear" Bloom <kevin@Eponine.local>
2015-09-20 21:07:47 -0400

    Added comments & fixed the start function

--- langtons-ant.lisp
+++ langtons-ant.lisp
@@ -1,7 +1,29 @@
-(defparameter *black-spaces* '())
-(defparameter *steps* nil)
-(defparameter *index* 0)
+;;This program is Free Software! Do with it as you wish!
+;;I'm sure you can run and edit this program without using
+;;non-free software, so lets just say it's licensed under
+;;the GPLv3 and above.
 
+;;This program is a simulation of the class mathematical
+;;idea of Langton's Ant. There is not GUI, but you can
+;;put the values found in the *black-spaces* list into
+;;a spreadsheet or calculator and see what you get.
+
+;;Please note that within the *black-spaces* list there
+;;will be a bunch of numbers that *don't* look like
+;;coordinates. This is normal, each pair of numbers
+;;starting from the beginning are coordinates.
+;;For example '(0 0 1 2 3 4), (0,0) is a coordinate of
+;;a black space; so if (1,2) and (3,4). Enjoy.
+
+(defparameter *black-spaces* '()) ;contains all blacked coordinates
+(defparameter *steps* nil) 
+(defparameter *index* 0) ;used in the remove-black function
+
+;;takes the first element, compares to x, if it's not equal
+;;the those coordinates are out so we pass that same list
+;;back, but without the first ordered pair (cddr blacklist).
+;;if it is equal to x, then check for y, if not send the
+;;list minus that ordered pair. If yes, pass true!
 (defun check-black(x y blacklist)
   (cond ((equal x (car blacklist))
 	 (cond ((equal y (cadr blacklist))
@@ -13,6 +35,8 @@
 	(t
 	 (check-black x y (cddr blacklist)))))
 
+;;basically the check-black function but keeps track of the index
+;;this makes it easy to remove coordinates
 (defun remove-black(x y blacklist)
   (cond ((equal x (car blacklist))
 	 (if (equal y (cadr blacklist))
@@ -31,11 +55,13 @@
   (push y *black-spaces*)
   (push x *black-spaces*))
 
+;;made keeping track of the steps easier
 (defun steps()
   (if (equal *steps* 0)
       'nil
       't))
 
+;;moving logic, the power of cond!
 (defun move(x y direction)
   (setf *steps* (- *steps* 1))
   (if (steps)
@@ -65,7 +91,10 @@
 			       (move (- x 1) y 'left))))))
       'done))
 
-;this blows up
+;;enter the x and y coordinates you'd like to start at
+;;enter 'b for black, 'w for white (or anything but 'b)
+;;enter 'up, 'right, 'down, or 'left for direction
+;;enter the number of steps you'd like the ant to make
 (defun start(x y b-or-w direction number-of-steps)
   (setf *steps* number-of-steps)
   (if (eq 'b b-or-w)
