7a320268d255ee04c4f519dee852bf577c74e90a
Kevin "The Nuclear" Bloom <kevin@Eponine.local>
2015-10-05 20:20:14 -0400

    Rewrote the entire thing for better efficiency and better Lisp syntax

--- langtons-ant.lisp
+++ langtons-ant.lisp
@@ -18,63 +18,52 @@
 
 (defparameter *black-spaces* '()) ;contains all blacked coordinates
 (defparameter *steps* nil) 
-(defparameter *index* 0) ;used in the remove-black function
 
-;;takes the first element, compares to x, if it's not equal
-;;the those coordinates are out so we pass that same list
-;;back, but without the first ordered pair (cddr blacklist).
-;;if it is equal to x, then check for y, if not send the
-;;list minus that ordered pair. If yes, pass true!
-(defun check-black(l blacklist)
-  (when (member l blacklist)
+;;Checks to see if the current position is a black one
+(defun check-black(l)
+  (when (member l *black-spaces*)
     'nil
     't))
 
-;;basically the check-black function but keeps track of the index
-;;this makes it easy to remove coordinates
-(defun remove-black(l blacklist)
-  (if (check-black l blacklist)
-      (remove l blacklist)
-      blacklist))
+;;The sole purpose of this is to make the code more readable
+(defun add-black(l)
+  (push l *black-spaces*))
 
-(defun add-black(x y)
-  (push y *black-spaces*)
-  (push x *black-spaces*))
-
-;;made keeping track of the steps easier
+;;Made keeping track of the steps easier
 (defun steps()
-  (when (equal *steps* 0)
-    'nil
-    't))
+  (if (equal *steps* 0)
+      'nil
+      't))
 
-;;moving logic, the power of cond!
-(defun move(x y direction)
+;;Movement logic function
+;;Very messy, working on making it look better!
+(defun move(l direction)
   (setf *steps* (- *steps* 1))
   (if (steps)
-      (progn (cond ((eq direction 'up)
-		    (if (check-black x y *black-spaces*) 
-			(progn (remove-black x y *black-spaces*) ;black
-			       (move (- x 1) y 'left))
-			(progn (add-black x y) ;white
-			       (move (+ x 1) y 'right))))
-		   ((eq direction 'right)
-		    (if (check-black x y *black-spaces*) 
-			(progn (remove-black x y *black-spaces*) ;black
-			       (move x (+ y 1) 'up))
-			(progn (add-black x y) ;white
-			       (move x (- y 1) 'down))))
-		   ((eq direction 'left)
-		    (if (check-black x y *black-spaces*)
-			(progn (remove-black x y *black-spaces*) ;black
-			       (move x (- y 1) 'down))
-			(progn (add-black x y) ;white
-			       (move x (+ y 1) 'up))))
-		   ((eq direction 'down)
-		    (if (check-black x y *black-spaces*)
-			(progn (remove-black x y *black-spaces*) ;black
-			       (move (+ x 1) y 'right))
-			(progn (add-black x y)
-			       (move (- x 1) y 'left))))))
+      (cond ((eq direction 'up)
+	     (if (check-black l) 
+		 (progn (setf *black-spaces* (remove l *black-spaces*)) ;black
+			(move (- l 1) 'left))
+		 (progn (add-black l) ;white
+			(move (+ l 1) 'right))))
+	    ((eq direction 'right)
+	     (if (check-black l) 
+		 (progn (setf *black-spaces* (remove l *black-spaces*)) ;black
+			(move (+ l #C(0 1)) 'up))
+		 (progn (add-black l) ;white
+			(move (- l #C(0 1)) 'down))))
+	    ((eq direction 'left)
+	     (if (check-black l)
+		 (progn (setf *black-spaces* (remove l *black-spaces*)) ;black
+			(move (- l #C(0 1)) 'down))
+		 (progn (add-black l) ;white
+			(move (+ l #C(0 1)) 'up))))
+	    ((eq direction 'down)
+	     (if (check-black l)
+		 (progn (setf *black-spaces* (remove l *black-spaces*)) ;black
+			(move (+ l 1) 'right))
+		 (progn (add-black l)
+			(move (- l 1) 'left)))))
       'done))
 
 ;;enter the x and y coordinates you'd like to start at
@@ -82,8 +71,11 @@
 ;;enter 'up, 'right, 'down, or 'left for direction
 ;;enter the number of steps you'd like the ant to make
 (defun start(x y b-or-w direction number-of-steps)
-  (setf *steps* number-of-steps)
-  (setf *black-spaces* '()) ;just in case you run this after you ran it before
-  (if (eq 'b b-or-w)
-      (add-black x y)
-      (move x y direction)))
+  (let ((c (complex x y)))
+    (setf *steps* number-of-steps)
+    (setf *black-spaces* '()) ;just in case you run this after you ran it before
+    (if (eq 'b b-or-w)
+	(progn (add-black c)
+	       (move c direction))
+	(move c direction))))
+
