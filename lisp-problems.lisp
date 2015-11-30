118c270f6fa4569d9179091ce5ea3ddd2f62d924
Kevin Bloom <13bloom.kevin@gmail.com>
2015-11-29 20:37:59 -0500

    Finished problems 9, 10 & 11

--- lisp-problems.lisp
+++ lisp-problems.lisp
@@ -57,12 +57,46 @@
 	    (compress rest-of-list)
 	    (append (cons current nil) (compress rest-of-list))))))
 
-;; 9 NOT COMPLETE
+;; 9
 (defun pack (orig-list)
   (if (null orig-list)
       nil
-      (let ((current (car orig-list)) (rest-of-list (cdr orig-list)))
-	(if (equal current (car rest-of-list))
-	    (cons (append (cons current nil) (cons (car rest-of-list) nil)) (pack rest-of-list))
-	    (pack rest-of-list)))))
+      (let ((fir-elem (car orig-list)) (sec-elem (cadr orig-list)))
+	(if (listp fir-elem)
+	    (if (equal (car fir-elem) sec-elem)
+		(pack
+		 (cons
+		  (cons sec-elem fir-elem) (cddr orig-list))) ;fir-elem is a list here
+		(cons fir-elem (pack (cdr orig-list))))
+	    (if (equal fir-elem sec-elem)
+		(pack
+		 (cons (list fir-elem sec-elem) (cddr orig-list))) ;jump 2 elements when passing list
+		(pack
+		 (cons (list fir-elem) (cdr orig-list))))))))
 
+;; 10
+(defun encode-logic (L)
+  (if (null L)
+      nil
+      (let ((elem (car L)))		;elem is a list
+	(cons
+	 (list (length elem) (car elem)) (encode-logic (cdr L))))))
+  
+(defun encode (L)
+  (encode-logic (pack L)))		;pack is from problem 9
+
+;; 11
+(defun encode-mod-logic (L)
+  (if (null L)
+      nil
+      (let ((elem (car L)))
+	(let ((len (length elem)))	;2 lexical variables?!?!? Far out!
+	  (if (equal 1 len)
+	      (cons (car elem) (encode-mod-logic (cdr L)))
+	      (cons
+	       (list (length elem) (car elem)) (encode-mod-logic (cdr L))))))))
+
+(defun encode-mod (L)
+  (encode-mod-logic (pack L)))		;remember pack?
+
+;; 12
