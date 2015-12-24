8dbc798ac0b5cb3bd3e5664980d498f487072ec2
Kevin Bloom <13bloom.kevin@gmail.com>
2015-12-24 15:48:01 -0500

    Added some informative comments to the big prime program

--- big-prime.lisp
+++ big-prime.lisp
@@ -9,9 +9,27 @@
 ;; thousands of digits, you could use this. It would just take forever!
 
 ;; It isn't written the best but it is 12 times more efficient than the original!
+;; In order to make the program much faster, I will be adding a global list that
+;; that contains some primes in which you can start at. For example, if you wanted
+;; to find the 1,000,500th prime, you could start a 1,000,000 and work the extra
+;; 500. (I probably won't add this feature)
 
 (load "/home/kevin/development/lisp/Common-Lisp/big-number.lisp")
 
+;; This function just returns the remainder of the division. How it works is
+;; rather complex.
+;; Say we are taking '(1 2 0) / '(3).
+;; We grab the first digit from the divisor and compare it to the dividend.
+;; If the dividend is bigger, we can't do anything yet so we cons the next
+;; digit with the first and compare again. Like so:
+;; '(3) > '(1) => '(3) < '(1 2)
+;; Once we have the second case, we use the find-quotient function to find
+;; the best possible quotient for the given numbers. Then we multiply, then
+;; subtract and this becomes our new remainder.
+;; At the end of the function there are some precautionary bits. These
+;; protect against weird cases where at the end of the division the
+;; remainder is equal to the dividend or if the remainder is 0 before
+;; we find the quotient.
 (defun mod-divide-loop (dividend divisor cur-div remainder)
   (if (null divisor)
       remainder
@@ -27,12 +45,19 @@
 		    no-zero-cur-div)
 		(mod-divide-loop dividend rest-of-div (append cur-div (list (cadr divisor))) remainder))))))
 	    
-
+;; To avoid confusing and making the above function more complex, if they are
+;; the same, it's 1.
 (defun mod-divide (dividend divisor)
   (if (equal dividend divisor)
       '(1)
       (mod-divide-loop dividend divisor (list (car divisor)) '(0))))
 
+;; Crunch through some numbers to see if the number is prime. The limit variable
+;; is nice because it cuts down on the amount of work needed to be done. What it
+;; does is takes the number divided by a number, say 3. This means that it isn't
+;; divisible by 3; but it also means that it isn't divisible by list-number / 3
+;; rounded up. We do this until the limit value is equal to or over the number
+;; we are dividing by.
 (defun prime-finder (list-number factor limit)
   (if (or (equal factor limit) (compare limit factor))
       t
@@ -43,6 +68,11 @@
 		t
 		(prime-finder list-number (addition factor '(2)) new-limit))))))
 
+;; If we have counted "pos" amounts of primes, we have made it to the prime we want!
+;; We check to see if the number is divisible by 2, if so, we already know it isn't
+;; a prime. However, if it isn't divisible by 2, we can conclude that it isn't
+;; divisible by ANY even number. This is why we pass a 3 and the prime-finder
+;; function increments by 2.
 (defun big-prime-loop (cur-num cur-prime-num pos l) ;l is a loop variable
   (cond ((equal pos l)
 	 cur-prime-num)
@@ -53,6 +83,8 @@
 	(t
 	 (big-prime-loop (addition cur-num '(1)) cur-prime-num pos l))))
 
+;; To work with the new method of prime-finding, we cannot have 2 or 3 go into
+;; big-prime-loop. This explains the use of the cond.
 (defun big-prime (position)
   (cond ((equal 1 position)
 	 '(2))
