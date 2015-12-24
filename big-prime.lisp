15a1ddb85e78b1d0d091755bf2a18892e2c9e578
Kevin Bloom <13bloom.kevin@gmail.com>
2015-12-24 15:17:11 -0500

    Big prime is now ~12x more efficient than the original

--- big-prime.lisp
+++ big-prime.lisp
@@ -8,6 +8,8 @@
 ;; than using this program. However, if were to be looking for a prime number with
 ;; thousands of digits, you could use this. It would just take forever!
 
+;; It isn't written the best but it is 12 times more efficient than the original!
+
 (load "/home/kevin/development/lisp/Common-Lisp/big-number.lisp")
 
 (defun mod-divide-loop (dividend divisor cur-div remainder)
@@ -20,7 +22,7 @@
 		(mod-divide-loop dividend rest-of-div
 				 (append new-remainder (list (cadr divisor))) new-remainder)))
 	    (if (null rest-of-div)
-		(if (equal dividend no-zero-cur-div)
+		(if (or (equal dividend no-zero-cur-div) (null no-zero-cur-div))
 		    '(0)
 		    no-zero-cur-div)
 		(mod-divide-loop dividend rest-of-div (append cur-div (list (cadr divisor))) remainder))))))
@@ -31,22 +33,25 @@
       '(1)
       (mod-divide-loop dividend divisor (list (car divisor)) '(0))))
 
-(defun prime-finder (list-number factor limit) ;limit is half the value of the list-number
-  (if (equal factor limit)
+(defun prime-finder (list-number factor limit)
+  (if (or (equal factor limit) (compare limit factor))
       t
       (if (equal '(0) (mod-divide factor list-number))
 	  nil
 	  (let ((new-limit (divide factor list-number)))
 	    (if (equal factor new-limit)
 		t
-		(prime-finder list-number (addition factor '(1)) new-limit))))))
+		(prime-finder list-number (addition factor '(2)) new-limit))))))
 
 (defun big-prime-loop (cur-num cur-prime-num pos l) ;l is a loop variable
-  (if (equal pos l)
-      cur-prime-num
-      (if (prime-finder cur-num '(2) cur-num)
-	  (big-prime-loop (addition cur-num '(1)) cur-num pos (+ l 1)) ;only increment loop when it IS prime
-	  (big-prime-loop (addition cur-num '(1)) cur-prime-num pos l))))
+  (cond ((equal pos l)
+	 cur-prime-num)
+	((equal '(0) (mod-divide '(2) cur-num))
+	 (big-prime-loop (addition cur-num '(1)) cur-prime-num pos l))
+	((prime-finder cur-num '(3) cur-num)
+	 (big-prime-loop (addition cur-num '(1)) cur-num pos (+ l 1))) ;only increment loop when it IS prime
+	(t
+	 (big-prime-loop (addition cur-num '(1)) cur-prime-num pos l))))
 
 (defun big-prime (position)
   (cond ((equal 1 position)
